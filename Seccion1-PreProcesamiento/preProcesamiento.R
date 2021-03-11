19ee1648bbca579a625983f2fba986abe93394dd
Rafael Ortiz <rortizr@unbosque.edu.co>
2021-03-11 14:42:52 -0500

    valores na

--- Seccion1-PreProcesamiento/preProcesamiento.R
+++ Seccion1-PreProcesamiento/preProcesamiento.R
@@ -5,3 +5,12 @@
 
 #Importar dataset
 dataset <- read.csv('datasets/Part1-Data_Preprocessing/Data.csv')
+
+#Reemplazar valores desconocidos
+dataset$Age <- ifelse(is.na(dataset$Age),
+                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Age)
+
+dataset$Salary <- ifelse(is.na(dataset$Salary),
+                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Salary)
--- Seccion1-PreProcesamiento/preProcesamiento.py
+++ Seccion1-PreProcesamiento/preProcesamiento.py
@@ -15,10 +15,21 @@ import pandas as pd
 #Importar el dataset
 dataset = pd.read_csv('~/Documents/udemy/machine-learning/datasets/Part1-Data_Preprocessing/Data.csv');
 
+#Reemplazar valores desconocidos
+
 #Todas las columnas, excepto la última
 X = dataset.iloc[:,:-1].values
+
+#Ultima columna
 y = dataset.iloc[:,3].values
 
+#Tratamiento de los NAs
+from sklearn.impute import SimpleImputer
+
+#axis 0 tomará los valores de la columna, 1 de la fila
+imputer = SimpleImputer(missing_values = np.nan, strategy = "mean", verbose= 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 
 
